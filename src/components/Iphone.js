7b4803e597ab169f07bb845f1c9429297e042608
abdelhakim-nk <abdelhakim-nk>
2021-10-08 01:45:52 +0200

    hakim.V0.4

---
+++ src/components/Iphone.js
@@ -0,0 +1,19 @@
+import './Iphone.css'
+import imagesIphone from './images/iphoneX.png'
+import { Parallax } from 'react-parallax';
+
+function Iphone() {
+    return (
+        <div>
+    <Parallax>
+        <div className="wrapper">
+            <img src={imagesIphone}/>
+        </div>
+    </Parallax>
+    </div>
+    )
+}
+
+
+
+export default Iphone 
---
+++ src/components/Team.js
@@ -0,0 +1,34 @@
+import imagesHakim from './images/Hakim.png'
+import imagesAnass from './images/Anass.jpg'
+import imagesEddine from './images/Eddine.jpg'
+import imagesAissetou from './images/Aissetou.jpg'
+import './Team.css'
+
+function Team() {
+    return(
+        <div>
+            <header>
+                <h2>Notre équipe</h2>
+            </header>
+        <section id="emps">
+            <figure class="emp">
+                <img src={imagesHakim} class="member boy" />
+                <figcaption>Hakim</figcaption>
+            </figure>
+            <figure class="emp">
+                <img src={imagesAnass} class="member boy" />
+                <figcaption>Anass</figcaption>
+            </figure>
+            <figure class="emp">
+                <img src={imagesEddine} class="member boy" />
+                <figcaption>Alley-Eddine</figcaption>
+            </figure>
+            <figure class="emp">
+                <img src={imagesAissetou} class="member boy" />
+                <figcaption>Aîssetou</figcaption>
+            </figure>
+        </section>
+        </div>
+    )
+}
+export default Team 
