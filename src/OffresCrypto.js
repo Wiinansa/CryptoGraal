be3828b2b5fefd37c0ddbbc3aa5e017138e7dfa4
Alley-eddine <alleybenhassine@gamil.com>
2021-10-07 15:02:37 +0200

    FirstCommit

---
+++ src/OffresCrypto.js
@@ -0,0 +1,83 @@
+
+import './OffresCrypto.css'
+
+function OffreCrypto(){
+    return(
+        <div className="Offres">
+
+            <div className="Cartes">
+
+                <h1>Gratuit</h1>
+                <h4>Pas de minimum</h4>
+                <h3>0 rabais</h3>
+
+                <ul>0 rabais</ul>
+                <ul>Livraison gratuite en Europe</ul>
+                <ul>Commandez n'importe où sur notre plateforme</ul>
+                <ul>Pas de limite de commande</ul>
+                <ul>Pas de revente autorisée</ul>
+
+                <div className="btn">
+                    Sign up
+                </div>
+
+            </div>
+
+            <div className="Cartes">
+
+                <h1>Dynamique</h1>
+                <h4>1 SPI</h4>
+                <h3>JUSQU'À 5% DE RÉDUCTION</h3>
+                
+                <ul>Plan de mise à l'échelle dynamique jusqu'à 5% de réduction sur Shopping.io</ul>
+                <ul>Chaque $SPI compte pour des remises supplémentaires</ul>
+                <ul>Livraison gratuite en Europe</ul>
+                <ul>Pas de limite de commande</ul>
+                <ul>Pas de revente autorisée</ul> 
+
+                <div className="btn">
+                    Sign up
+                </div>
+
+            </div>
+
+            <div className="Cartes">
+
+                <h1>VIP</h1>
+                <h4>1000 SPI</h4>
+                <h3>10% DE RÉDUCTION</h3>
+
+                <ul>0 rabais</ul>
+                <ul>Livraison gratuite en Europe</ul>
+                <ul>Commandez n'importe où sur notre plateforme</ul>
+                <ul>Pas de limite de commande</ul>
+                <ul>Pas de revente autorisée</ul>
+
+                <div className="btn">
+                    Sign up
+                </div>
+
+            </div>
+
+            <div className="Cartes">
+
+                <h1>Vrai Homme</h1>
+                <h4>1000 SPI</h4>
+                <h3>BIENTÔT DISPONIBLE</h3>
+
+                <ul>0 rabais</ul>
+                <ul>Livraison gratuite en Europe</ul>
+                <ul>Commandez n'importe où sur notre plateforme</ul>
+                <ul>Pas de limite de commande</ul>
+                <ul>Pas de revente autorisée</ul>
+
+                <div className="btn">
+                    Sign up
+                </div>
+            </div>
+        </div>        
+
+    );
+}
+
+export default OffreCrypto;
